86db88ec7c590d49449035a4a4b5abe1e612b7b5
Wang-chengfei <2677373210@qq.com>
2021-12-08 15:59:57 +0800

    12.8

--- paperBase.sql
+++ paperBase.sql
@@ -1,12 +1,7 @@
-create table user
-(
-    id varchar(32) not null primary key
-);
-
 create table paper
 (
     id      int auto_increment primary key,
-    type    int          null,
+    type    varchar(32) null,
     content varchar(512) null,
     optionA varchar(256) null,
     optionB varchar(256) null,
@@ -15,5 +10,35 @@ create table paper
     optionE varchar(256) null,
     optionF varchar(256) null,
     answer  varchar(6)   null,
-    score   int          null
+    score   int          null,
+    creator varchar(256) null,
+);
+
+create table mentality_paper
+(
+    id      int auto_increment primary key,
+    type    varchar(32) null,
+    exam_id varchar(64)  null,
+    content varchar(512) null,
+    optionA varchar(256) null,
+    optionB varchar(256) null,
+    optionC varchar(256) null,
+    optionD varchar(256) null,
+    optionE varchar(256) null,
+    optionF varchar(256) null,
+    answer  varchar(6)   null,
+    score   int          null,
+    creator varchar(256) null,
+    foreign key (exam_id) references mentality_exam(id)
+);
+
+create table mentality_exam
+(
+    id       varchar(64) primary key,
+    section1 varchar(2048),
+    section2 varchar(2048),
+    section3 varchar(2048),
+    section4 varchar(2048),
+    section5 varchar(2048),
+    creator  varchar(256)
 );
